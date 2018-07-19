676aef66dbe9aad03eaeb045a5ad074cc92771ed
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-19 20:26:13 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,17 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
+winner = ""
 
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers.each do |k, v|
+    if k == suite_a: && name.start_with?("A")
+      winner = name
+    end
 end
+
+winner 
